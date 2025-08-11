179e60d9db221e0e550cc5f08280d3778014ba5c
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-08-11 12:26:49 -0300

    configurando db

--- cadastro/cadastro/settings.py
+++ cadastro/cadastro/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+user = os.getenv('user')
+senha = os.getenv('password')
+port = os.getenv('port')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,7 +81,10 @@ WSGI_APPLICATION = 'cadastro.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django'
+        'NAME': 'django',
+        'USER': f'{user}',
+        'PASSWORD': f'{senha}',
+        'PORT': f'{port}'
     }
 }
 
