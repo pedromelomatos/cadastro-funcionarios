88ccccc2a93608075d591dff7761a2043b62343d
Pedro Henrique Melo Matos <melomatosp@gmail.com>
2025-08-12 13:35:21 -0300

    criação do app de autenticação

--- cadastro/cadastro/views.py
+++ cadastro/cadastro/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest
 
-def login(request):
-    return render(request, 'login.html')
+def redirecionar(request:HttpRequest):
+    return redirect('autenticacao:login')
